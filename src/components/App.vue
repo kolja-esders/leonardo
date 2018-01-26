f40c2a727cfa9ba1fe48cf68811ab558ad6c2671
Kolja Esders <kolja.esders@gmail.com>
2018-01-26 17:46:03 +0100

    Style updates

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
-    <custom-comp/>
+    <!--<img class="leonardo" src="../assets/leonardo.png">-->
+    <question/>
+    <question/>
+    <question/>
   </div>
 </template>
 
 <script>
+import Question from './Question.vue'
+
 export default {
   name: 'app',
+  components: { Question },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -21,13 +26,40 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 3rem;
+}
+
+.leonardo {
+  display: block;
+  margin: 0 auto;
+  width: 200px;
+  margin-bottom: 3rem;
+  border-radius: 100px;
+  transition: 0.8s;
+
+  &:hover {
+    box-shadow: 0 0 30px rgba(0, 0, 0, 0.3);
+    transition: 0.2s;
+  }
+}
+
+body {
+  background: radial-gradient(ellipse at top left, #137F4B 0%, #006736 100%) no-repeat;
+  min-height: 100vh;
+  margin: 0;
+}
+
+h1 {
+  font-size: 4rem;
+  margin-top: 1rem;
+  margin-top: 3rem;
+  color: #242628;
 }
 
 h1, h2 {
-  font-weight: normal;
+  font-weight: 900;
+  text-align: center;
 }
 
 ul {
---
+++ src/components/Question.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <p>test</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'question',
+
+  props: {
+    question: {
+      type: String,
+      required: false,
+    },
+  }
+
+}
+</script>
+
+<style lang="scss">
+.container {
+  padding: .5rem 1.5rem .5rem 1.5rem;
+  background-color: #f0f0f0ff;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
+  border-radius: 2px;
+  margin-bottom: 1rem;
+}
+</style>
