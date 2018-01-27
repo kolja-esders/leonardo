bd3431102e37a9cf2dda9d7dca5af9c573885592
Kolja Esders <kolja.esders@gmail.com>
2018-01-27 15:43:37 +0100

    Render markdown in questions

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,19 +1,20 @@
 <template>
     <div class="box question">
       <div class="content">
-        <p>
-        <strong class="is-size-4">{{ content }}</strong>
-        <span class="is-pulled-right">
-          <span v-if="!answer" class="tag is-danger">Unresolved</span>
-        </span>
-          <br>
-          {{ answer }}
+        <p class="is-clearfix">
+          <strong class="is-size-4">{{ content }}</strong>
+          <span class="is-pulled-right">
+            <span v-if="!answer" class="tag is-danger">Unresolved</span>
+          </span>
+          <span v-html="markdownAnswer"></span>
         </p>
       </div>
     </div>
 </template>
 
 <script>
+import marked from 'marked';
+
 export default {
   name: 'question',
 
@@ -25,7 +26,12 @@ export default {
       type: String,
       required: false,
     }
-  }
+  },
+  computed: {
+    markdownAnswer() {
+      return marked(this.answer || '');
+    }
+  },
 
 }
 </script>
