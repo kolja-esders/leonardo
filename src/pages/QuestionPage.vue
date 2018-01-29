7082533a5a5c52ed05882fc4e01f11464fa6992d
Kolja Esders <kolja.esders@gmail.com>
2018-01-29 22:01:37 +0100

    Pages dir

---
+++ src/pages/QuestionPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="question-page">
+  </div>
+</template>
+
+<script>
+import Question from '../components/Question.vue'
+import axios from 'axios';
+
+export default {
+  name: 'question-page',
+  components: { Question },
+}
+</script>
+
+<style lang="scss">
+</style>
