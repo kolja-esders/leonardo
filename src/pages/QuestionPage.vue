efaa542c7e5f6d0e817db8b454fb81babf99e43c
Kolja Esders <kolja.esders@gmail.com>
2018-01-30 03:14:49 +0100

    Tmp: Same font on question page

--- src/pages/QuestionPage.vue
+++ src/pages/QuestionPage.vue
@@ -27,6 +27,10 @@ export default {
 <style lang="scss">
 
 .question-page {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
   padding-top: 3rem;
   color: #2c3e50;
 }
